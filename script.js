c31dc4aee4b115e0090a25a0e7ed0bfc79e3e1d7
Jericho Pasco <112312834+Jpasco31@users.noreply.github.com>
2023-08-17 16:19:22 +0800

    Re-work return for playRound function
    Change return to a specific to win, tie or lose string

--- script.js
+++ script.js
@@ -10,32 +10,32 @@ function playRound(playerChoice, comChoice) {
     playerChoice = playerChoice.toLowerCase();
 
     if(playerChoice === comChoice){
-        return "Its a tie you both chose " + comChoice;
+        return "tie";
     } else if (playerChoice === "rock") {
         if(comChoice === "paper"){
-            return "You Lose! " + comChoice + " beats " + playerChoice;
+            return "lose";
         } else if (comChoice === "scissors"){
-            return "You Win! " + playerChoice + " beats " + comChoice;
+            return "win";
         }
     } else if (playerChoice === "paper") {
         if(comChoice === "scissors"){
-            return "You Lose! " + comChoice + " beats " + playerChoice;
+            return "lose";
         } else if (comChoice === "rock"){
-            return "You Win! " + playerChoice + " beats " + comChoice;
+            return "win";
         }
     } else if (playerChoice === "scissors"){
         if(comChoice === "rock"){
-            return "You Lose! " + comChoice + " beats " + playerChoice;
+            return "lose"
         } else if (comChoice === "paper"){
-            return "You Win! " + playerChoice + " beats " + comChoice;
+            return "win";
         }
     } 
 
     return "Not a valid choice";
 }
 
-const computerSelection = getComputerChoice();
-const playerSelection = prompt("Rock, paper or scissors? Choose");
+// const computerSelection = getComputerChoice();
+// const playerSelection = prompt("Rock, paper or scissors? Choose");
 
 
-console.log(playRound(playerSelection, computerSelection));
+// console.log(playRound(playerSelection, computerSelection));
