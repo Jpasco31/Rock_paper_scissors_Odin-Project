31f68c821d6e61bbdcbe842260d7087e5087348c
Jericho Pasco <112312834+Jpasco31@users.noreply.github.com>
2023-08-17 16:05:37 +0800

    Create playRound function
    This function simulates a round of rock paper scissors

--- script.js
+++ script.js
@@ -4,3 +4,38 @@ function getComputerChoice(){
     const comChoiceNum = Math.floor(Math.random() * 3);
     return computerOptions[comChoiceNum]; 
 }
+
+
+function playRound(playerChoice, comChoice) {
+    playerChoice = playerChoice.toLowerCase();
+
+    if(playerChoice === comChoice){
+        return "Its a tie you both chose " + comChoice;
+    } else if (playerChoice === "rock") {
+        if(comChoice === "paper"){
+            return "You Lose! " + comChoice + " beats " + playerChoice;
+        } else if (comChoice === "scissors"){
+            return "You Win! " + playerChoice + " beats " + comChoice;
+        }
+    } else if (playerChoice === "paper") {
+        if(comChoice === "scissors"){
+            return "You Lose! " + comChoice + " beats " + playerChoice;
+        } else if (comChoice === "rock"){
+            return "You Win! " + playerChoice + " beats " + comChoice;
+        }
+    } else if (playerChoice === "scissors"){
+        if(comChoice === "rock"){
+            return "You Lose! " + comChoice + " beats " + playerChoice;
+        } else if (comChoice === "paper"){
+            return "You Win! " + playerChoice + " beats " + comChoice;
+        }
+    } 
+
+    return "Not a valid choice";
+}
+
+const computerSelection = getComputerChoice();
+const playerSelection = prompt("Rock, paper or scissors? Choose");
+
+
+console.log(playRound(playerSelection, computerSelection));
